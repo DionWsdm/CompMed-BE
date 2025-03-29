ae0d2aeed50f6d70f3d78b102f2f2282865dcbaa
DionWsdm <dionwisdom29@gmail.com>
2025-03-30 02:10:00 +0700

    Test menggunakan db url public

--- src/config/knexfile.ts
+++ src/config/knexfile.ts
@@ -7,11 +7,8 @@ const config: { [Key: string]: Knex.Config }  = {
     development: {
         client: "pg",
         connection: {
-            host: process.env.DB_HOST,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME,
-            port: Number(process.env.DB_PORT),
+            connectionString: process.env.DB_URL,
+            ssl: false,
         },
         migrations: {
             directory: "./migrations",
